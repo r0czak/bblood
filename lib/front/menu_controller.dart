94eb752ba1ce5cd816c37e1c04c6409c1988a8b1
matim1324 <matim1324@gmail.com>
2021-10-28 04:09:16 +0200

    Created menu bar and simple navigation between app pages.

---
+++ lib/front/menu_controller.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '/front/home_page.dart';
+import '/front/blood_card_page.dart';
+import '/front/research_page.dart';
+
+class MenuController extends StatefulWidget {
+  const MenuController({Key? key}) : super(key: key);
+
+  @override
+  _MenuControllerState  createState() => _MenuControllerState();
+}
+
+class _MenuControllerState extends State<MenuController>{
+
+  final PageController _pageController = PageController();
+  final List<Widget> _screen = [HomeScreen(), BloodCardScreen(), ResearchScreen()];
+
+  int _selectedIndex = 0;
+  void _onPageChanged(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onItemTapped(int selectedIndex){
+    _pageController.jumpToPage(selectedIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: _screen,
+        onPageChanged: _onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _selectedIndex == 0 ? Colors.red : Colors.grey,
+            ),
+            title: Text(
+              "Główna",
+              style: TextStyle(color: _selectedIndex == 0 ? Colors.red : Colors.grey,),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bloodtype,
+              color: _selectedIndex == 1 ? Colors.red : Colors.grey,
+            ),
+            title: Text(
+              "Krew",
+              style: TextStyle(color: _selectedIndex == 1 ? Colors.red : Colors.grey,),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.location_on,
+              color: _selectedIndex == 2 ? Colors.red : Colors.grey,
+            ),
+            title: Text(
+              "Punkty krwi",
+              style: TextStyle(color: _selectedIndex == 2 ? Colors.red : Colors.grey,),
+            ),
+          ),
+          /* BottomNavigationBarItem(
+            icon: Icon(Icons.redeem),
+            title: Text("Zniżki"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            title: Text("Badania"),
+          ),*/
+        ],
+      ),
+    );
+  }
+}
