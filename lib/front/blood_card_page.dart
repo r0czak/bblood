d62d146a6e7d53a0d1eb59a7965f17ff3f7725ae
matim1324 <matim1324@gmail.com>
2021-11-26 01:40:26 +0100

    ADd blood donor info page.

--- lib/front/blood_card_page.dart
+++ lib/front/blood_card_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bblood/front/user_profile_page.dart';
+import 'package:bblood/widgets/blood_card_widget.dart';
 
 class BloodCardScreen extends StatefulWidget {
   const BloodCardScreen({Key? key}) : super(key: key);
@@ -9,30 +10,65 @@ class BloodCardScreen extends StatefulWidget {
 }
 
 class _BloodCardScreenState extends State<BloodCardScreen> {
-  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      //backgroundColor: const Color(0xFFDA4148),
       backgroundColor: const Color(0xFFEDEDED),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: const Color(0xFFEDEDED),
-            child: Padding(
-              padding: const EdgeInsets.all(45.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                    children: <Widget>[
-                      Text("Dane oddawanej krwi, ilsoc karty")
-                    ]
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+          child: Column(
+              children: [
+                Container(
+                    height: 200,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text("Twoja ostatnia donacja", style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text("Czwartek, 11.11.2021r.", style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                        SizedBox(height: 5,),
+                        Text("Krew pełna", style: TextStyle(fontSize: 18)),
+                        SizedBox(height: 5,),
+                        Text("500ml", style: TextStyle(fontSize: 24)),
+                        SizedBox(height: 10,),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFFDCDCDC),
+                          ),
+                          child: Text("Ciśnienie: 128/71          Puls: 7/29"),
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.location_on),
+                            Text("RCKiK w Lodzi"),
+                          ],
+                        ),
+                      ],
+                    ),
                 ),
-              ),
+                Divider(color: Colors.black),
+                Expanded(
+                  child: CardWidget("1500"),
+                ),
+              ],
             ),
-          ),
         ),
       ),
     );
