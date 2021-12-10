c0e7093cf33811f71f1ff08a48280b6139514e91
matim1324 <matim1324@gmail.com>
2021-12-10 07:52:12 +0100

    Upload news image to news widget.

--- lib/viewmodels/home_view_model.dart
+++ lib/viewmodels/home_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:bblood/enums/view_state.dart';
 import 'package:bblood/models/blood_levels_model.dart';
 import 'package:bblood/models/news_info_model.dart';
+import 'package:bblood/services/firebase_storage_service.dart';
 import 'package:bblood/viewmodels/base_model.dart';
 
 import '../locator.dart';
@@ -9,6 +10,7 @@ import '../services/firestore_service.dart';
 class HomeViewModel extends BaseModel {
   //final authService = locator<AuthService>();
   final _firestoreService = locator<FirestoreService>();
+  final _firebaseStorageService = locator<FirebaseStorageService>();
   late BloodLevelsModel levels;
   late List<NewsInfoModel> news;
 
@@ -32,5 +34,12 @@ class HomeViewModel extends BaseModel {
 
   List<NewsInfoModel> getNews() {
     return news;
+  }
+
+  Future<String> getNewsImageURL(String image) async {
+    setState(ViewState.busy);
+    String imageUrl = await _firebaseStorageService.getNewsImageURL(image);
+    setState(ViewState.idle);
+    return imageUrl;
   }
 }
